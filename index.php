c70a0e4baf6425b6676522fa14b922fc2cabd950
Тарасова Анастасия Александровна <23135@step.edu>
2016-03-22 20:24:51 +0200

    123

--- index.php
+++ index.php
@@ -7,6 +7,8 @@ if ("/index.php" === $uri || "/" === $uri) {
     list_action();
 } else if ("/index.php/show/" === $uri && isset($_GET['id'])) {
     show_action($_GET['id']);
+} else if ("/index.php/add/" === $uri) {
+    add_action();
 } else {
     header('Status: 404 Not Found');
     echo "<html><body><h1>Page Not Found $uri</h1></body></html>";
