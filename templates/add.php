4751738e76bdff890eb858f0a7a1adc063386e73
Тарасова Анастасия Александровна <23135@step.edu>
2016-03-22 20:52:18 +0200

    111111

--- templates/add.php
+++ templates/add.php
@@ -3,7 +3,7 @@
     <form action= "index.php/create/post/" method= "POST"> 
         <p>Message: </p><p> <input type="text" name= "title"></input></p> 
         <textarea name="text"></textarea>
+        <input type="submit" value="Add"/> 
     </form>
-    <input type= "submit" value= "Add"> 
 <?php $content = ob_get_clean()?>
 <?php include 'layout.php'?>
